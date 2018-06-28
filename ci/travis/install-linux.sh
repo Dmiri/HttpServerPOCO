95e7d57c584252e3d3d2e23acb76287aad50cd8a
Dmitriy <MuravyovDmitri@gmail.com>
2018-06-28 05:01:51 +0300

    test(gcc and g++ 7) for unix

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,13 +1,9 @@
-sudo add-apt-repository -y ppa:jonathonf/gcc-7.1
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/stable
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-7
+gcc-7 --version
+
 sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main"
-sudo apt-get update -qq
-
-sudo apt-get install -qq g++-5
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
-
-sudo apt-get install -qq gcc-7
-
 sudo apt-get update
 sudo apt-get install -y clang-6.0
 
