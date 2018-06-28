08baa5bb7a4c66955c216a1c44457044a4a498cf
Dmitriy <MuravyovDmitri@gmail.com>
2018-06-28 03:39:48 +0300

    test(direct) for unix

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,3 +1,9 @@
+sudo add-apt-repository ppa:jonathonf/gcc-7.1
 sudo apt update
-sudo apt-get install cmake gcc-7 python-pip
+
+sudo apt-get -y install gcc-7 g++-7
+
+sudo apt-get -y install cmake python-pip
+
+pip install --upgrade pip
 sudo -H pip install conan
