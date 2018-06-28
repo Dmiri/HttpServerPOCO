e0829f043e69e04bb15babc7ac644bfad7855ed1
Dmitriy <MuravyovDmitri@gmail.com>
2018-06-28 04:11:14 +0300

    test(gcc-5 and g++-5 -y) for unix

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,10 +1,11 @@
-sudo add-apt-repository -y ppa:jonathonf/gcc-7.1
+# sudo add-apt-repository -y ppa:jonathonf/gcc-7.1
 sudo apt update
 
-sudo apt-get -y install gcc-7 g++-7
-
+sudo apt-get -y install gcc-5 g++-5
+CC = gcc-5
+CXX = g++-5
 sudo apt-get -y install cmake python-pip
-
+gcc -xc++ -E -v -
 pip install --upgrade pip
 sudo -H pip install conan
 echo openssl trouble
