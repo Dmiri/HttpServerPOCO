a49b8820e4e2d54f35e6cbb78c86d813d87fe64e
Dmitriy <MuravyovDmitri@gmail.com>
2018-06-28 16:43:47 +0300

    test(gcc and g++ 7update.v2) for unix

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,30 +1,22 @@
+echo info cmake
+sudo apt-get install cmake
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt update
-sudo apt install g++-7 -y
-sudo update-alternatives —install /usr/bin/gcc gcc /usr/bin/gcc-7 60 \
-                                                    —slave /usr/bin/g++ g++ /usr/bin/g++-7
-sudo update-alternatives —config gcc
-gcc —version
-g++ - version
+sudo apt --force-yes  install g++-7 -y
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 \
+                         --slave /usr/bin/g++ g++ /usr/bin/g++-7 
+sudo update-alternatives --config gcc
+echo info gcc
+gcc --version
+g++ --version
 
 # sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main"
 # sudo apt update
 # sudo apt-get install -y clang-6.0
 
-sudo apt-get install cmake
+echo info conan
 
-sudo apt-get -y install cmake python-pip
-pip install --upgrade pip
-pip install conan
-
-
-
-
-
-
-# sudo add-apt-repository -y ppa:ubuntu-toolchain-r/stable
-# sudo apt update
-# sudo apt -y install gcc-5 g++-5
-# sudo apt-get -y install gcc-7 g++-7
-# gcc-7 --version
-# g++-7 --version
+sudo apt-get install python-pip -y
+sudo pip install --upgrade pip
+sudo -H pip install conan --upgrade
